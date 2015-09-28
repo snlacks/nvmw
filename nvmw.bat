2b0b5f451a7d3cd738f9894d3b3cb21721f33ac6
Steven Lacks <snlacks@users.noreply.github.com>
2015-09-28 15:02:09 -0400

    Removed a debugging echo.

--- nvmw.bat
+++ nvmw.bat
@@ -139,7 +139,6 @@ if %NODE_TYPE% == iojs (
       set NODE_EXE_URL=%NVMW_NODEJS_ORG_MIRROR%/%NODE_VERSION%/node.exe
     )
   ) else (
-    echo HELLO %NODE_VERSION%
     if not "%NODE_VERSION:~1,1%" == "0" (
       set NODE_EXE_URL=%NVMW_NODEJS_ORG_MIRROR%/%NODE_VERSION%/win-x64/node.exe
     ) else (
